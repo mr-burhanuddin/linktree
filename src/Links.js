760df32bbc275fabfb00e1383a36a3b79a44c74e
mr-burhanuddin <bjpresswala@gmail.com>
2022-05-30 19:54:05 +0530

    updated animated and styles

--- src/Links.js
+++ src/Links.js
@@ -1,15 +1,22 @@
-import React from "react";
-import "./App.css";
+import React from 'react';
+import './App.css';
+import { motion } from 'framer-motion/dist/framer-motion';
 
 function Links({ path, logo, alt }) {
   return (
     <>
-      <a href={path}>
-        <div className="link__container">
-          <img className="link__logo" src={logo} alt={alt} />
-          <div className="link__name">{alt}</div>
-        </div>
-      </a>
+      <motion.div
+        className='link'
+        whileHover={{ scale: 1.2 }}
+        whileTap={{ scale: 0.8 }}
+      >
+        <a href={path}>
+          <div className='link__container'>
+            <img className='link__logo' src={logo} alt={alt} />
+            <div className='link__name'>{alt}</div>
+          </div>
+        </a>
+      </motion.div>
     </>
   );
 }
